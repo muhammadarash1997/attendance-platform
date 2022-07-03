df45e3bf842cb217fd7c6cb7f579d382abef94f2
muhammadarash1997 <muhammadarash1997@gmail.com>
2022-07-03 08:43:47 +0800

    add some tag for validation

--- dto/dto.go
+++ dto/dto.go
@@ -3,22 +3,22 @@ package dto
 import "time"
 
 type EmployeeDTO struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
-	Name     string `json:"name"`
-	Token    string `json:"token"`
+	ID       string `json:"id" validate:"required"`
+	Username string `json:"username" validate:"required"`
+	Name     string `json:"name" validate:"required"`
+	Token    string `json:"token" validate:"required"`
 }
 
 type AttendanceDTO struct {
-	ID      string    `json:"id"`
-	EmployeeID  string    `json:"employee_id"`
-	InDate  *time.Time `json:"in_date"`
-	OutDate *time.Time `json:"out_date"`
+	ID         string     `json:"id" validate:"required"`
+	EmployeeID string     `json:"employee_id" validate:"required"`
+	InDate     *time.Time `json:"in_date"`
+	OutDate    *time.Time `json:"out_date"`
 }
 
 type ActivityDTO struct {
-	ID           string `json:"id"`
-	EmployeeID       string `json:"employee_id"`
-	AttendanceID string `json:"attendance_id"`
-	Note         string `json:"note"`
+	ID           string `json:"id" validate:"required"`
+	EmployeeID   string `json:"employee_id" validate:"required"`
+	AttendanceID string `json:"attendance_id" validate:"required"`
+	Note         string `json:"note" validate:"required"`
 }
--- dto/request.go
+++ dto/request.go
@@ -1,22 +1,22 @@
 package dto
 
 type RegisterRequest struct {
-	Username     string `json:"username"`
-	Name         string `json:"name"`
-	Password string `json:"password"`
+	Username string `json:"username" validate:"required"`
+	Name     string `json:"name" validate:"required"`
+	Password string `json:"password" validate:"required"`
 }
 
 type LoginRequest struct {
-	Username    string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
 }
 
 type CreateActivityRequest struct {
-	EmployeeID       string `json:"employee_id"`
-	AttendanceID string `json:"attendance_id"`
-	Note         string `json:"note"`
+	EmployeeID   string `json:"employee_id" validate:"required"`
+	AttendanceID string `json:"attendance_id" validate:"required"`
+	Note         string `json:"note" validate:"required"`
 }
 
 type UpdateActivityRequest struct {
-	Activity ActivityDTO	`json:"activity"`
+	Activity ActivityDTO `json:"activity"`
 }
